f1d826ef788213c07fdc3db232dd22b821acbc55
Mathias Fredriksson <mafredri@gmail.com>
2014-08-03 21:09:01 +0300

    Set up virtualenv in post-install script.

--- source/CONTROL/post-install.sh
+++ source/CONTROL/post-install.sh
@@ -1,19 +1,37 @@
 #!/bin/sh
 
-APKG_PKG_DIR=/usr/local/AppCentral/deluge
+if [ -z $APKG_PKG_DIR ]; then
+    PKG_DIR=/usr/local/AppCentral/deluge
+else
+    PKG_DIR=$APKG_PKG_DIR
+fi
 
-case "$APKG_PKG_STATUS" in
+setup_virtualenv() {
+    (cd ${PKG_DIR};
+        mv lib/python2.7/site-packages ./
+        virtualenv .
+        mv site-packages/* lib/python2.7/site-packages && rmdir site-packages
+        source bin/activate
+        # Install deluge executables in the virtualenv
+        easy_install deluge)
+}
+
+case "${APKG_PKG_STATUS}" in
 	install)
-        source $APKG_PKG_DIR/CONTROL/env.sh
+        setup_virtualenv
+
+        # Source env variables
+        source ${PKG_DIR}/CONTROL/env.sh
 
         # If previous configurations don't exist, copy the initial template
-        if [ ! -d $DELUGED_CONF ]; then
-            mkdir -p $DELUGED_CONF
-            cp $APKG_PKG_DIR/config/* $DELUGED_CONF/
-            chown -R admin:administrators $DELUGED_CONF
+        if [ ! -d ${DELUGED_CONF} ]; then
+            mkdir -p ${DELUGED_CONF}
+            cp ${PKG_DIR}/config/* ${DELUGED_CONF}/
+            chown -R ${DELUGED_USER} ${DELUGED_CONF}
         fi
 		;;
 	upgrade)
+        setup_virtualenv
 		;;
 	*)
 		;;
