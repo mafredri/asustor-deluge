93b951d8a1c332ebe78e86477d2fd15a0ca24153
Mathias Fredriksson <mafredri@gmail.com>
2014-12-13 13:43:57 +0200

    clean up on uninstall (remove configuration directory).

---
+++ source/CONTROL/pre-uninstall.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ -z $APKG_PKG_DIR ]; then
+    PKG_DIR=/usr/local/AppCentral/deluge
+else
+    PKG_DIR=$APKG_PKG_DIR
+fi
+
+# Source env variables
+source ${PKG_DIR}/CONTROL/env.sh
+
+rm -rf ${DELUGED_CONF}
+
+exit 0
