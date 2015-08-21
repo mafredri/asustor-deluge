9285b325f83d92b67227d061ccb889ac8e8e9798
Mathias Fredriksson <mafredri@gmail.com>
2015-08-21 22:41:35 +0300

    Remove python dep, fix start-stop script

--- source/CONTROL/start-stop.sh
+++ source/CONTROL/start-stop.sh
@@ -3,26 +3,27 @@
 NAME="Deluge"
 PACKAGE="deluge"
 
-if [ -z $APKG_PKG_DIR ]; then
+if [ -z "${APKG_PKG_DIR}" ]; then
 	PKG_DIR=/usr/local/AppCentral/${PACKAGE}
 else
 	PKG_DIR=$APKG_PKG_DIR
 fi
 
-source ${PKG_DIR}/CONTROL/env.sh
+. "${PKG_DIR}/CONTROL/env.sh"
 
 export PATH=${PKG_DIR}/bin:$PATH
 export LD_LIBRARY_PATH=${PKG_DIR}/lib:${LD_LIBRARY_PATH}
 export HOME=${DELUGED_USER_HOME}
 
+DELUGED_LOGLEVEL=info
+DELUGE_WEB_LOGLEVEL=info
+
 DELUGED="${PKG_DIR}/bin/deluged"
 DELUGED_LOG="${DELUGED_CONF}/deluged.log"
 DELUGED_PID="${DELUGED_CONF}/deluged.pid"
-DELUGED_OPTS="--config ${DELUGED_CONF} --logfile ${DELUGED_LOG} --loglevel info"
 DELUGE_WEB="${PKG_DIR}/bin/deluge-web"
 DELUGE_WEB_PID="${DELUGED_CONF}/deluge-web.pid"
 DELUGE_WEB_LOG="${DELUGED_CONF}/deluge-web.log"
-DELUGE_WEB_OPTS="--config ${DELUGED_CONF} --logfile ${DELUGE_WEB_LOG} --loglevel info"
 
 USER=${DELUGED_USER%:*}
 CHUID=${DELUGED_USER}
@@ -31,39 +32,50 @@ start_daemon() {
     # Set umask to create files with world r/w
     umask 0
 
-	start-stop-daemon -S --quiet --pidfile ${DELUGED_PID} --chuid "${CHUID}" --user ${USER} --exec ${DELUGED} -- \
-		--quiet --pidfile ${DELUGED_PID} ${DELUGED_OPTS}
+	start-stop-daemon -S --quiet --pidfile "${DELUGED_PID}" --chuid "${CHUID}" \
+		--user "${USER}" --exec "${DELUGED}" -- \
+		--quiet --pidfile "${DELUGED_PID}" --config "${DELUGED_CONF}" \
+		--logfile "${DELUGED_LOG}" --loglevel "${DELUGED_LOGLEVEL}"
 
-	start-stop-daemon -S --quiet --background --pidfile ${DELUGE_WEB_PID} --make-pidfile --chuid "${CHUID}" \
-		--user ${USER} --exec ${DELUGE_WEB} -- --quiet ${DELUGE_WEB_OPTS}
+	start-stop-daemon -S --quiet --background --pidfile "${DELUGE_WEB_PID}" \
+		--make-pidfile --chuid "${CHUID}" --user "${USER}" \
+		--exec "${DELUGE_WEB}" -- \
+		--quiet --config "${DELUGED_CONF}" --logfile "${DELUGE_WEB_LOG}" \
+		--loglevel "${DELUGE_WEB_LOGLEVEL}"
 }
 
 stop_daemon() {
-	start-stop-daemon -K --quiet --user ${USER} --pidfile ${DELUGED_PID}
-	start-stop-daemon -K --quiet --user ${USER} --pidfile ${DELUGE_WEB_PID}
+	start-stop-daemon -K --quiet --user "${USER}" --pidfile "${DELUGED_PID}"
+	start-stop-daemon -K --quiet --user "${USER}" --pidfile "${DELUGE_WEB_PID}"
 
 	wait_for_status 1 20
 
 	if [ $? -eq 1 ]; then
 		echo "Taking too long, killing service..."
-		start-stop-daemon -K --signal 9 --quiet --user ${USER} --pidfile ${DELUGED_PID}
-		start-stop-daemon -K --signal 9 --quiet --user ${USER} --pidfile ${DELUGE_WEB_PID}
+		start-stop-daemon -K --signal 9 --quiet --user "${USER}" --pidfile "${DELUGED_PID}"
+		start-stop-daemon -K --signal 9 --quiet --user "${USER}" --pidfile "${DELUGE_WEB_PID}"
 	fi
 }
 
 daemon_status() {
-    start-stop-daemon -K --quiet --test --user ${USER} --pidfile ${DELUGED_PID}
-    DELUGED_RETVAL=$?
-    start-stop-daemon -K --quiet --test  --user ${USER} --pidfile ${DELUGE_WEB_PID}
-    DELUGE_WEB_RETVAL=$?
-    [ ${DELUGED_RETVAL} -eq 0 -a ${DELUGE_WEB_RETVAL} -eq 0 ] || return 1
+	start-stop-daemon -K --quiet --test --user "${USER}" --pidfile "${DELUGED_PID}"
+	ret1=$?
+	start-stop-daemon -K --quiet --test  --user "${USER}" --pidfile "${DELUGE_WEB_PID}"
+	ret2=$?
+
+    if [ $ret1 -eq 0 ] || [ $ret2 -eq 0 ]; then
+    	return 0
+    fi
+
+    return 1
 }
 
 wait_for_status() {
     counter=$2
-    while [ ${counter} -gt 0 ]; do
-        daemon_status
-        [ $? -eq $1 ] && return
+    while [ "${counter}" -gt 0 ]; do
+        if ! daemon_status; then
+        	return 0
+        fi
         counter=$(( counter - 1 ))
         sleep 1
     done
@@ -72,11 +84,11 @@ wait_for_status() {
 
 case $1 in
 	start)
-		if daemon_status; then
-            echo "${NAME} is already running"
-        else
+		if ! daemon_status; then
             echo "Starting ${NAME}..."
             start_daemon
+        else
+            echo "${NAME} is already running"
         fi
 		;;
 
